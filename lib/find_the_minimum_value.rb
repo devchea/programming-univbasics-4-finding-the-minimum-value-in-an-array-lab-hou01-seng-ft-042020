87d5821dd7b3fac8703a44b1285925e04d545681
Richard Chea <rchea817@gmail.com>
2020-04-09 23:05:04 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,12 @@
+require "pry"
 def find_min_value(array)
   # Add your solution here
  i = 0
  min_value = array[0]
  while i < array.length do
+   binding.pry
    if min_value < array[i]
+     binding.pry
      min_value = array[i]
    end
    i += 1
